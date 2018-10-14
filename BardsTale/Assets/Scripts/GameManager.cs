198da836db2732ca2c9ed3fbc4a6dd96af8517c0
BuildTools <ss2sc@virginia.edu>
2018-10-14 14:49:21 -0400

    Roooom
    rooooooooooooooooooooooooom(ba)

--- BardsTale/Assets/Scripts/GameManager.cs
+++ BardsTale/Assets/Scripts/GameManager.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    public Room room;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        room = GetComponent<Room>();
+        InitGame();
 	}
+
+    void InitGame()
+    {
+        room.SetupScene();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
---
+++ BardsTale/Assets/Scripts/Room.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+    public int columns = 12;
+    public int rows = 8;
+
+    public GameObject floorTile;
+    public GameObject wallTile;
+
+    private List<Vector3> gridPositions = new List<Vector3>();
+    private Transform roomHolder;
+
+    void InitializeList()
+    {
+        gridPositions.Clear();
+
+        for ( int i = 1; i < columns - 1; i++)
+        {
+            for ( int j = 1; j < rows - 1; j++)
+            {
+                gridPositions.Add(new Vector3(i, j, 0f));
+            }
+        }
+    }
+
+    void roomSetup() {
+        roomHolder = new GameObject("Room").transform;
+
+        for (int i = -1; i < columns + 1; i++)
+        {
+            for (int j = -1; j < rows + 1; j++)
+            {
+                GameObject toInstantiate = floorTile;
+                if (i == -1 || i == columns || j == -1 || j == rows)
+                    toInstantiate = wallTile;
+
+                GameObject instance = Instantiate(toInstantiate, new Vector3(i, j, 0f), Quaternion.identity) as GameObject;
+
+                instance.transform.SetParent(roomHolder);
+            }
+        }
+    }
+
+    public void SetupScene()
+    {
+        roomSetup();
+        InitializeList();
+
+    }
+}
